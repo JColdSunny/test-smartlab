859acddf620ea8ebcf4125834bbf8ff45cefb970
Dzmitry Bahaslouski <jcoldsun.tech@gmail.com>
2024-04-24 15:15:50 +0300

    clear

--- src/main/java/com/jcs/interview/Application.java
+++ src/main/java/com/jcs/interview/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InterviewApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(InterviewApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
